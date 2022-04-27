c0fb1109f245e4a3973f8d1ca023a419e62e92eb
SaulPaez03 <saulpaez03@gmail.com>
2022-04-27 17:45:19 -0400

    Redux config

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Route,
   Routes,
   useRoutes,
   BrowserRouter as Router,
+  useNavigate,
 } from "react-router-dom";
+import { setUser } from "./app/Reducers/userSlice";
 import CompleteSignUp from "./Components/CompleteSignUp/CompleteSignUp";
 import Content from "./Components/Content/Content";
 import Navbar from "./Components/Navbar/Navbar";
+import { QuestionCreate } from "./Components/QuestionCreate.tsx/QuestionCreate";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useAppSelector, useAppDispatch } from "./app/hooks";
 const App = () => {
+  const { isAuthenticated, user } = useAuth0();
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const DBUser = useAppSelector((state) => state.user);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      dispatch(setUser(user?.id));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isAuthenticated && DBUser.user_name === "") {
+      navigate(`/Profile/${user?.id}/edit`);
+    }
+  }, [DBUser]);
+
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/Content" element={<Content />} />
         <Route path="/CompleteSignUp" element={<CompleteSignUp />} />
+        <Route path="/Ask" element={<QuestionCreate />} />
       </Routes>
     </>
   );
---
+++ src/app/Reducers/userSlice.ts
@@ -0,0 +1,60 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { useAppSelector } from "../hooks";
+import axios from "axios";
+interface User {
+  first_name: string;
+  last_name: string;
+  email: string;
+  country: string;
+  city: string;
+  role: number;
+  user_name: string;
+  profile_picture: string;
+  biography: string;
+  password: string;
+  posts: Array<string>;
+  answers: Array<string>;
+  comments: Array<string>;
+  own_henry_coin: number;
+  give_henry_coin: number;
+  theoric: Array<string>;
+  excersices: Array<string>;
+}
+
+const initialState: User = {
+  first_name: "",
+  last_name: "",
+  email: "",
+  country: "",
+  city: "",
+  role: 0,
+  user_name: "",
+  profile_picture: "",
+  biography: "",
+  password: "",
+  posts: [],
+  answers: [],
+  comments: [],
+  own_henry_coin: 0,
+  give_henry_coin: 0,
+  theoric: [],
+  excersices: [],
+};
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<string>) => {
+      let id = action.payload;
+      axios
+        .get(`/users/${id}`)
+        .then((res) => res.data)
+        .then((data) => {
+          state = data;
+        })
+        .catch((e) => console.log(e));
+    },
+  },
+});
+export const { setUser } = userSlice.actions;
+export default userSlice.reducer;
