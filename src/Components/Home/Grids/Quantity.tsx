6bedec1c04d5f902e06ac545615e91b7c83372a4
Q4Tr073 <nicolas.aguilar73@hotmail.com>
2022-05-13 00:58:13 -0300

    feature footer compnents

--- src/Components/Home/Grids/Quantity.tsx
+++ src/Components/Home/Grids/Quantity.tsx
@@ -11,56 +11,6 @@ export default function Quantity() {
   // const exerc = useAppSelector((state) => state.exercises)
   const dispatch = useAppDispatch();
 
-<<<<<<< HEAD
-export default function Quantity(){
-    const [theoric, setTheoric] = useState([]);
-    const post = useAppSelector((state: any) => state.getAllPosts);
-    // const exerc = useAppSelector((state) => state.exercises)
-    const dispatch = useAppDispatch();
-
-    
-
-    useEffect(() => {dispatch(fetchGetAllPosts(10))
-        fetchAllTheorics().then((res: any) => setTheoric(res));
-    }, []);
-    // useEffect(() => {dispatch(getAllExercises())});
-    
-
-    return(
-        <Container>
-            <Grid container spacing={5}>
-                <Grid item xs={12} sm={4}>
-                    <Typography
-                        variant='h4'
-                        component='div'
-                        display='flex'
-                        justifyContent='center'>
-                            {`+${theoric ? theoric.length : null}`}
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} sm={4}>
-                    <Typography
-                        variant='h4'
-                        component='div'
-                        display='flex'
-                        justifyContent='center'>
-                            {/* {`+${theoric ? theoric.length : null}`} */}
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} sm={4}>
-                    <Typography
-                        variant='h4'
-                        component='div'
-                        display='flex'
-                        justifyContent='center'>
-                            {`+${post ? post.posts.length : null}`}
-                    </Typography>
-                </Grid>
-            </Grid>
-        </Container>
-    )
-};
-=======
   useEffect(() => {
     dispatch(fetchGetAllPosts(10));
     fetchAllTheorics().then((res: any) => setTheoric(res));
@@ -104,4 +54,3 @@ export default function Quantity(){
     </Container>
   );
 }
->>>>>>> 50fa75564996b673f39d719c949ee9454a9349bc
